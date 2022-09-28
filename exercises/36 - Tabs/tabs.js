27399cddf881f573923fd783823931f1942ae379
Snaz22 <s.naylor1502@gmail.com>
2022-09-28 22:36:05 +0100

    Finished tab exercise.

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,27 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
+const tabPanelArray = Array.from(tabPanels);
+
+function handleTabClick(event) {
+  // hide all panels
+  tabPanels.forEach((panel) => {
+    panel.hidden = true;
+  });
+  // mark all tabs as unselected
+  tabButtons.forEach((tab) => {
+    tab.setAttribute('aria-selected', false);
+  });
+  // mark clicked tab as selected
+  event.currentTarget.setAttribute('aria-selected', true);
+  // find correct tab panel and show it
+  const { id } = event.currentTarget;
+  const tabPanel = tabPanelArray.find(
+    (panel) => panel.getAttribute('aria-labelledby') === id
+  );
+  tabPanel.hidden = false;
+}
+
+tabButtons.forEach((button) =>
+  button.addEventListener('click', handleTabClick)
+);
