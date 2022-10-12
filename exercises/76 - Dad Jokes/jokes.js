2bbb765a3a37e935837a23e306f87b12da0ef06e
Snaz22 <s.naylor1502@gmail.com>
2022-10-12 23:34:26 +0100

    Jokes API exercise.

--- exercises/76 - Dad Jokes/jokes.js
+++ exercises/76 - Dad Jokes/jokes.js
@@ -8,3 +8,37 @@ const buttonText = [
   'please stop',
   'that was the worst one',
 ];
+
+const jokeButton = document.querySelector('.getJoke');
+const jokeHolder = document.querySelector('.joke p');
+
+async function fetchJoke() {
+  const response = await fetch('https://icanhazdadjoke.com', {
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+  const data = await response.json();
+  // console.log(data.joke);
+  return data;
+}
+
+function randomItemFromArray(arr, not) {
+  const item = arr[Math.floor(Math.random() * arr.length)];
+  // console.log(item);
+  if (item === not) {
+    return randomItemFromArray(arr, not);
+  }
+  return item;
+}
+
+async function handleClick() {
+  const { joke } = await fetchJoke();
+  jokeHolder.textContent = joke;
+  jokeButton.textContent = randomItemFromArray(
+    buttonText,
+    jokeButton.textContent
+  );
+}
+
+jokeButton.addEventListener('click', handleClick);
